f8cc344f4c6b27ada9bf23766dd9b4a371e1d4a7
charlyjose <23473681+charlyjose@users.noreply.github.com>
2020-01-23 04:43:01 +0530

    major changes

--- server.py
+++ server.py
@@ -1,68 +1,33 @@
-#!flask/bin/python
 from flask import Flask, request, jsonify, send_from_directory, render_template
 from pymongo import MongoClient
-import time
-import test
+from pymongo.errors import ServerSelectionTimeoutError
+import sys
+import json
+
+with open('config/config.json') as f:
+        config = json.load(f)
 
 app = Flask(__name__)
-# app = Flask(__name__, static_url_path='')
-# app = Flask(__name__, template_folder='templates')
 
-
-
-# client = MongoClient("mongodb://root:12345@localhost:27017")
-# db = client.testdb
-# sensor = db.sensorData
-
-client = MongoClient("mongodb://root:12345@localhost:27017/")
-db = client["testdb"]
-collection = db["sensorData"]
-
-
-
-
-
-# @app.route('/')
-# def hello():
-#     return "Hello From Server"
-
-
-@app.route("/")
-def main():
-    return render_template('data.html', reload = time.time())
-
-@app.route("/api/info")
-def api_info():
-    info = {
-       "ip" : "127.0.0.1",
-       "hostname" : "everest",
-       "description" : "Main server",
-       "load" : [ 3.21, 7, 14 ]
-    }
-    return jsonify(info)
-
-@app.route("/api/calc")
-def add():
-    cursor = collection.find().sort("_id", -1).limit(1)
-    for document in cursor:
-        temp = document["temp"]
-        humidity = document["humidity"]
-        uv = document["uv"]
-        moisture = document["moisture"]
-
-    return jsonify({
-        "temp"      :  temp,
-        "humidity"  :  humidity,
-        "uv"        :  uv,
-        "moisture"  :  moisture
-})
-
-
-
+try:
+    connect = "mongodb://" + config['user'] + ":" + config['password'] + "@" + config['host'] + ":" + config['port']  
+    client = MongoClient(connect, serverSelectionTimeoutMS=10, connectTimeoutMS=20000)
+    info = client.server_info()
+    print("Connected to MongoDB")
+    db = client["testdb"]
+    collection = db["sensorData"]
+except ServerSelectionTimeoutError:
+    print("MongoDB connection failed")
+    sys.exit()
 
 
 @app.route('/farmSense', methods=['GET'])
 def farmSense():
+    humidity = None
+    temp = None
+    uv = None
+    moisture = None
+
     # Fetch latest data
     cursor = collection.find().sort("_id", -1).limit(1)
     for document in cursor:
@@ -71,15 +36,13 @@ def farmSense():
         humidity = document["humidity"]
         uv = document["uv"]
         moisture = document["moisture"]
-
-    # return render_template('test.html')
     return render_template('test.html', temp=temp, humidity=humidity, uv=uv, moisture=moisture)
 
 
-
-
-
-
+@app.route('/sense', methods=['GET'])
+def sense():
+    endpoint = 'http://blynk-cloud.com/dHeSr2A_yh9uwwfzz8rlHhcgB1GcTygy/project'
+    return render_template('farmSense.html', ENDPOINT=endpoint)
 
 
 @app.route('/pushData', methods=['POST'])
@@ -87,41 +50,27 @@ def sensorData():
     if (request.is_json):
         content = request.get_json()
 
-        print ("Temperature: ", content["temp"])
-        print ("Humidity: ", content["humidity"])
-        print ("UV: ", content["uv"])
-        print ("Moisture: ", content["moisture"])
-        
+        print("Temperature: ", content["temp"])
+        print("Humidity: ", content["humidity"])
+        print("UV: ", content["uv"])
+        print("Moisture: ", content["moisture"])
+
         temp = content["temp"]
         humidity = content["humidity"]
         uv = content["uv"]
         moisture = content["moisture"]
 
-        print ("\n\n\nTEST VALUE", test.sample(temp, humidity), "\n\n\n")
-
-        collection.insert_one({"temp":temp, "humidity":humidity, "uv":uv, "moisture":moisture})
+        collection.insert_one(
+            {"temp": temp, "humidity": humidity, "uv": uv, "moisture": moisture})
 
         return "ok"
     else:
         return "error"
 
 
-
-
-
-
-
-
 @app.route('/<path:path>', methods=['GET'])
 def staticFiles(path):
-    #last = sensor.findOne({}, { sort: { _id: -1 }, limit: 1 });
-    #print (last)
-    #return '<html><p>WORKING</p></html>'
-    return send_from_directory('static', path)
-
-
-
-
+    return send_from_directory('public', path)
 
 
 if __name__ == '__main__':
--- test.py
+++
@@ -1,7 +0,0 @@
-
-def sample(a, b):
-    a=int(a)
-    b=int(b)
-    a+=10
-    b+=10
-    return [a, b]
